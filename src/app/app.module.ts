67b795b99904e3883c1bcc5da843e4bef7075825
Victor Mann <victorcmann@gmail.com>
2019-02-16 14:07:33 -0200

    componentes base e organizacao do projeto em pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DadosUsuarioComponent } from './dados-usuario/dados-usuario.component';
+import { ListaUsuarioComponent } from './lista-usuario/lista-usuario.component';
+import { DetalheUsuarioComponent } from './detalhe-usuario/detalhe-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DadosUsuarioComponent,
+    ListaUsuarioComponent,
+    DetalheUsuarioComponent
   ],
   imports: [
     BrowserModule
